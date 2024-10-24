4f5102aca5574af898c908e04ac604ce75434a33
jmeaster30 <jhneasterday09@gmail.com>
2024-10-24 01:37:20 -0400

    Finished makefile and should have pyproject finished too

--- hatch_build.py
+++
@@ -1,7 +0,0 @@
-from hatchling.metadata.plugin.interface import MetadataHookInterface
-
-import lilytk
-
-class VersionMetadataHook(MetadataHookInterface):
-  def update(self, metadata):
-    metadata["version"] = lilytk.__version__
